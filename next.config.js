e7b251860e11c443325d663410501542821625b6
Alexandre F <nightlyside@gmail.com>
2022-03-13 22:20:45 +0100

    Adding build prefix for production builds

--- next.config.js
+++ next.config.js
@@ -3,11 +3,14 @@
 import withPlugins from "next-compose-plugins";
 import optimizedImages from "next-optimized-images";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = withPlugins([[optimizedImages, {}]], {
 	reactStrictMode: true,
 	images: {
 		domains: ["cdn.discordapp.com"]
-	}
+	},
+	assetPrefix: isProd ? "https://nightlyside.github.io/bevy_showcase_website/" : ""
 });
 
 export default nextConfig;
